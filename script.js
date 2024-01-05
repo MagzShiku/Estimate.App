5add63e1e54e1bb6782386fe5cbfdcbd6b960f6a
Magdaline Njuguna <m2015njuguna@gmail.com>
2024-01-05 14:02:11 +0300

    adjustment 21

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ document.addEventListener("DOMContentLoaded", function() {
     showSlide();
     setInterval(nextSlide, 5000);
 });
+
+/*this is for the dropdown menu of click2 */
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var color = document.getElementById("color").value;
+    var animal = document.getElementById("animal").value;
+    var fruit = document.getElementById("fruit").value;
+    var vehicle = document.getElementById("vehicle").value;
+    var drink = document.getElementById("drink").value;
+
+    // Perform any desired actions with the selected values
+    console.log("Name: " + name);
+    console.log("Color: " + color);
+    console.log("Animal: " + animal);
+    console.log("Fruit: " + fruit);
+    console.log("Vehicle: " + vehicle);
+    console.log("Drink: " + drink);
+}
