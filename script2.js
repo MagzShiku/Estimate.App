34fe57db63b8620e1323eda0c964be3a2492c265
Magdaline Njuguna <m2015njuguna@gmail.com>
2024-01-06 16:33:08 +0300

    interractive6

--- script2.js
+++ script2.js
@@ -2,17 +2,23 @@ function submitForm() {
     var year = document.getElementById("year").value;
     var budget = document.getElementById("budget").value;
     var county = document.getElementById("county").value;
+    var adults = document.getElementById("adults").value;
+    var children = document.getElementById("children").value;
 
     // Perform any desired actions with the selected values
     console.log("year: " + year);
     console.log("budget: " + budget);
     console.log("county: " + county);
+    console.log("adults: " + adults);
+    console.log("children: " + children);
 
     // Redirect to the next page with the selected values as query parameters
     // window.location.href = "moreoptions.html?year=" + year;
     window.location.href = "moreoptions.html?year=" + encodeURIComponent(year) +
     "&budget=" + encodeURIComponent(budget) +
-    "&county=" + encodeURIComponent(county);
+    "&county=" + encodeURIComponent(county) +
+    "&adults=" + encodeURIComponent(adults) +
+    "&children=" + encodeURIComponent(children);
 
     // Prevent the form from submitting through the default action
     return false;
