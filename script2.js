3dfce62a9987bc84ddd8836e985c7af1c1487ca7
Magdaline Njuguna <m2015njuguna@gmail.com>
2024-01-06 17:04:03 +0300

    itsinterractive7

--- script2.js
+++ script2.js
@@ -1,16 +1,28 @@
+function selectTypology(typology) {
+    // Store the selected typology in the hidden input field
+    document.getElementById("selectedTypology").value = typology;
+
+    // You can also perform additional actions based on the selected typology
+    console.log('Selected typology:', typology);
+}
+
 function submitForm() {
     var year = document.getElementById("year").value;
     var budget = document.getElementById("budget").value;
     var county = document.getElementById("county").value;
     var adults = document.getElementById("adults").value;
     var children = document.getElementById("children").value;
+    var floors = document.getElementById("floors").value;
+
+    // Get the selected typology from the hidden input field
+    var typology = document.getElementById("selectedTypology").value;
 
     // Perform any desired actions with the selected values
     console.log("year: " + year);
     console.log("budget: " + budget);
     console.log("county: " + county);
     console.log("adults: " + adults);
-    console.log("children: " + children);
+    console.log("floors: " + floors);
 
     // Redirect to the next page with the selected values as query parameters
     // window.location.href = "moreoptions.html?year=" + year;
@@ -18,7 +30,9 @@ function submitForm() {
     "&budget=" + encodeURIComponent(budget) +
     "&county=" + encodeURIComponent(county) +
     "&adults=" + encodeURIComponent(adults) +
-    "&children=" + encodeURIComponent(children);
+    "&children=" + encodeURIComponent(children) +
+    "&floors=" + encodeURIComponent(floors) +
+    "&typology=" + encodeURIComponent(typology);
 
     // Prevent the form from submitting through the default action
     return false;
