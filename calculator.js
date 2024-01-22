762029ed7b103292f5d07b79ca4c8cb44b453ace
Magdaline Njuguna <m2015njuguna@gmail.com>
2024-01-22 15:12:26 +0300

    more and more updates

---
+++ calculator.js
@@ -0,0 +1,9 @@
+//GETS VALUE OF CLICK2//
+// Get the dropdown element
+const dropdown = document.getElementById('myDropdown');
+
+// Get the selected value
+const selectedValue = dropdown.value;
+
+// Compute the values at the end of the presentation
+console.log(selectedValue);
