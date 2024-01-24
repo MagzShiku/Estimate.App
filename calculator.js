e43de262c2f0de1fd2de6efbb3365baaac659b0e
Magdaline Njuguna <m2015njuguna@gmail.com>
2024-01-24 15:33:08 +0300

    uoda

--- calculator.js
+++ calculator.js
@@ -1,18 +1,24 @@
 function updateBudget() {
     // Get the selected budget value
-    var selectedBudget = document.getElementById('budget').value;
+    var selectedBudgetText = document.getElementById('budget').textContent;
+
+    // Convert the budget value to an integer
+    var selectedBudget = parseInt(selectedBudgetText);
+
+    // Format the budget value with decimal places and commas
+    var formattedBudget = selectedBudget.toLocaleString(undefined, { minimumFractionDigits: 2 });
 
     // Store the selected budget in localStorage
     localStorage.setItem('selectedBudget', selectedBudget);
 
     // Update the 'Y' value in the summary section
-    document.getElementById('budgetedCost').innerText = 'Ksh. ' + selectedBudget;
+    document.getElementById('budgetedCost').innerText = 'Ksh. ' + formattedBudget;
 }
 
 // Add this block to set the initial value
 document.addEventListener("DOMContentLoaded", function () {
     // Get the initially selected budget value
-    var initialBudget = document.getElementById('budget').value;
+    var initialBudget = document.getElementById('budget').textContent;
 
     // Store the initially selected budget in localStorage
     localStorage.setItem('selectedBudget', initialBudget);
